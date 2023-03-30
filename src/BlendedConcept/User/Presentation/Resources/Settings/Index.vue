4ea7070b5a6e241f38fd8e68eaab34a5e98849fe
zawzawwin-dev <dev1@hostmyanmar.net>
2023-03-30 14:54:48 +0630

    setup design for site settings

--- src/BlendedConcept/User/Presentation/Resources/Settings/Index.vue
+++ src/BlendedConcept/User/Presentation/Resources/Settings/Index.vue
@@ -4,35 +4,38 @@
         <div class="menu-items bg-cwhite col-span-2 text-center pt-10">
             <div class="items">
                 <ul>
-                    <li class="flex items-center justify-items-center" @click="settings('global')">
+                    <li :class="['flex items-center justify-items-center ',currentSettings =='global' ? 'border-l-8      border-l-primary':'']" @click="settings('global')">
+                        <span class="material-symbols-outlined p-3">
+                            settings_applications
+                        </span>
+                        Application
+                    </li>
+                    <li :class="['flex items-center justify-items-center ',currentSettings =='date_format' ? 'border-l-8 border-l-primary':'']" @click="settings('date_format')">
+                        <span class="material-symbols-outlined p-3">
+                            history_toggle_off
+                        </span>
+                        Date Format
+                    </li>
+
+                    <li :class="['flex items-center justify-items-center ',currentSettings =='banner' ? 'border-l-8 border-l-primary':'']" @click="settings('banner')">
+                        <span class="material-symbols-outlined p-3">
+                            vrpano
+                        </span>
+                        Banner
+                    </li>
+                    <!-- <li class="flex items-center gap-2 justify-items-center ">
                         <span class="material-symbols-outlined p-3">
                             settings
                         </span>
-                        global setting
-                    </li>
-                    <li class="flex items-center justify-items-center ">
-                        <span class="material-symbols-outlined p-3">
-                            settings
-                        </span>
-                        color setting
-                    </li>
-                    <li class="flex items-center gap-2 justify-items-center ">
-                        <span class="material-symbols-outlined p-3">
-                            settings
-                        </span>
-                        font setting
-                    </li>
-                    <li class="flex items-center gap-2 justify-items-center ">
-                        <span class="material-symbols-outlined p-3">
-                            settings
-                        </span>
-                        banner setting
-                    </li>
+                        Dashboard Mode
+                    </li> -->
                 </ul>
             </div>
         </div>
         <div class="container col-span-10 pt-10 pl-10">
             <Globle v-if="currentSettings=='global'"/>
+            <DateFormat v-if="currentSettings=='date_format'"/>
+            <Banner v-if="currentSettings == 'banner'"/>
         </div>
          </div>
     </AdminLayout>
@@ -41,7 +44,10 @@
 <script setup>
 import {ref} from "vue"
 import AdminLayout from "@dashboard/AdminLayout.vue";
-import Globle from "./globle.vue"
+import Globle from "./Globle.vue";
+import DateFormat from "./DateFormat.vue"
+import Banner from "./Banner.vue"
+
 let currentSettings = ref("");
 // globale setting function
 
