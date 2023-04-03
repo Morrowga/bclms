73ae21cdac05d3548526cdbfec3ef845eb01fe6f
zawzawwin-dev <dev1@hostmyanmar.net>
2023-04-03 08:40:30 +0630

    fixed merged conflict

--- src/BlendedConcept/Organization/Presentation/Resources/Index.vue
+++ src/BlendedConcept/Organization/Presentation/Resources/Index.vue
@@ -29,10 +29,10 @@
             <p class="text-primary text-[18px] font-normal">Student</p>
           </div>
         </div>
-        <div class="flex flex-col" v-for="i in 10" :key="i">
+        <div class="flex flex-col">
           <Image src="images/profile/profiletwo.png"/>
           <div class="description pl-4">
-            <h4 class="text-primary text-[20px] font-semibold leading-[150%]">Student three</h4>
+            <h4 class="text-primary text-[20px] font-semibold leading-[150%]">Student Two</h4>
             <p class="text-primary text-[18px] font-normal">Student</p>
           </div>
         </div>
