0f31300d9febc33c64e5eb9085270b82d240b08c
Aung-Myint-Myat-23711 <112838755+Aung-Myint-Myat-23711@users.noreply.github.com>
2023-04-07 11:05:20 +0630

    test-ui

--- src/BlendedConcept/User/Presentation/Resources/Portal/Portal.vue
+++ src/BlendedConcept/User/Presentation/Resources/Portal/Portal.vue
@@ -1,8 +1,19 @@
 <template>
   <FrontendLayout>
-    <h1>Portal Layout</h1>
+    <div>
+      <v-row class="align-stretch">
+        <v-col cols="12" md="6" class="d-flex">
+          <AnalyticsCongratulationsJohn class="flex-grow-1" />
+        </v-col>
+        <v-col cols="12" md="6" class="d-flex">
+          <EcommerceSalesOverview class="flex-grow-1" />
+        </v-col>
+      </v-row>
+    </div>
   </FrontendLayout>
 </template>
 <script setup>
 import FrontendLayout from "@Layouts/Portal/FrontendLayout.vue";
+import AnalyticsCongratulationsJohn from "@/views/dashboard/analytics/AnalyticsCongratulationsJohn.vue";
+import EcommerceSalesOverview from "@/views/dashboard/ecommerce/EcommerceSalesOverview.vue";
 </script>
