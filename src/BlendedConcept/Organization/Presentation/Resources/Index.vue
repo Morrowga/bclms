e5dd6625447ecc5c87c0565020d1091145d2628f
Aung-Myint-Myat-23711 <112838755+Aung-Myint-Myat-23711@users.noreply.github.com>
2023-04-28 16:01:44 +0630

    add-new-notifications-3

--- src/BlendedConcept/Organization/Presentation/Resources/Index.vue
+++ src/BlendedConcept/Organization/Presentation/Resources/Index.vue
@@ -1,36 +1,34 @@
 <template>
-    <!-- for superadmin dashboard check -->
-    <AdminLayout :user="user" :user_role="current_user_role">
-        <div v-if="current_user_role == 'BC Super Admin'">
-            <!-- alert announment box -->
-            <VAlert
-            prominent
-            variant="tonal"
-            type="error"
-            v-model="isAlertVisible"
-            closable
-            close-label="Close Alert"
-            >
-                <template #text>
-                   <div class="d-flex flex-column">
-                    <p>Error</p>
-                    <p>Error Message</p>
-                   </div>
-                </template>
-            </VAlert>
-            <!-- end announment  box-->
-            <SuperAdminDashboard
-                :orgainzations_users="props.orgainzations_users"
-            >
-            </SuperAdminDashboard>
-        </div>
-        <div v-else-if="current_user_role == 'BC Subscriber'">
-            <TeacherOrParentDashboard> </TeacherOrParentDashboard>
-        </div>
-        <div v-else>
-            <StaffDashboard></StaffDashboard>
-        </div>
-    </AdminLayout>
+  <!-- for superadmin dashboard check -->
+  <AdminLayout :user="user" :user_role="current_user_role">
+    <div v-if="current_user_role == 'BC Super Admin'">
+      <!-- alert announment box -->
+      <VAlert
+        prominent
+        variant="tonal"
+        type="error"
+        v-model="isAlertVisible"
+        closable
+        close-label="Close Alert"
+      >
+        <template #text>
+          <div class="d-flex flex-column">
+            <p>Error</p>
+            <p>Error Message</p>
+          </div>
+        </template>
+      </VAlert>
+      <!-- end announment  box-->
+      <SuperAdminDashboard :orgainzations_users="props.orgainzations_users">
+      </SuperAdminDashboard>
+    </div>
+    <div v-else-if="current_user_role == 'BC Subscriber'">
+      <TeacherOrParentDashboard> </TeacherOrParentDashboard>
+    </div>
+    <div v-else>
+      <StaffDashboard></StaffDashboard>
+    </div>
+  </AdminLayout>
 </template>
 
 <script setup>
@@ -44,11 +42,19 @@ import { defineProps } from "vue";
 import { usePage } from "@inertiajs/vue3";
 //## variable section
 let props = defineProps(["current_user_role", "user", "orgainzations_users"]);
-const isAlertVisible = ref(true)
+const isAlertVisible = ref(true);
 
 let notifications = computed(() => usePage().props.notifications?.data);
 let unread_notifications_count = computed(
-    () => usePage().props.unreadNotificationsCount
+  () => usePage().props.unreadNotificationsCount
 );
-console.log("noti", notifications.value);
+const removeNotification = (notificationId) => {
+  form.post(route("markAsRead", { id: notificationId }), {
+    onSuccess: () => {
+      notifications.value = notifications.value.filter(
+        (noti) => noti.id != notificationId
+      );
+    },
+  });
+};
 </script>
