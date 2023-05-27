fc465dbbe78ca894d99da39d4dcd4e3917df7c8e
zawzawwin-dev <dev1@hostmyanmar.net>
2023-05-27 09:03:21 +0630

    fixed notification

--- src/BlendedConcept/System/Application/Providers/SystemServiceProvider.php
+++ src/BlendedConcept/System/Application/Providers/SystemServiceProvider.php
@@ -7,17 +7,24 @@ use Src\Auth\Application\Repositories\Eloquent\DashboardRepository;
 use Src\Auth\Domain\Repositories\DashboardRepositoryInterface;
 use Src\BlendedConcept\System\Domain\Repositories\AnnouncementRepositoryInterface;
 use Src\BlendedConcept\System\Application\Repositories\Eloquent\AnnouncementRepository;
+use Src\BlendedConcept\System\Application\Repositories\Eloquent\NotificationRepository;
 use Src\BlendedConcept\System\Domain\Repositories\PageBuilderInterface;
 use Src\BlendedConcept\System\Application\Repositories\Eloquent\PageBuilderRepository;
 use Src\BlendedConcept\System\Domain\Repositories\SettingRepositoryInterface;
 use Src\BlendedConcept\System\Application\Repositories\Eloquent\SettingRepository;
 use Src\BlendedConcept\System\Application\Repositories\Eloquent\PlanRepository;
+use Src\BlendedConcept\System\Domain\Repositories\NotificationRepositoryInterface;
 use Src\BlendedConcept\System\Domain\Repositories\PlanRepositoryInterface;
 
 class SystemServiceProvider extends ServiceProvider
 {
     public function register()
     {
+
+        $this->app->bind(
+            NotificationRepositoryInterface::class,
+            NotificationRepository::class
+        );
 
         $this->app->bind(
             DashboardRepositoryInterface::class,
