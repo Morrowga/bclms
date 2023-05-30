edfecd14fe927664078f9a4c8d92cb1d9d62d3c7
hareom284 <dev1@hostmyanmar.net>
2023-05-30 15:44:12 +0630

    fixed setting policy static function

--- src/BlendedConcept/System/Application/Policies/SettingPolicy.php
+++ src/BlendedConcept/System/Application/Policies/SettingPolicy.php
@@ -5,30 +5,30 @@ namespace Src\BlendedConcept\System\Application\Policies;
 class SettingPolicy
 {
 
-    public function view()
+    public static function view()
     {
         return auth()->user()->hasPermission('access_settings');
     }
 
-    public function create()
+    public static function create()
     {
         return auth()->user()->hasPermission('create_user');
     }
-    public function store()
+    public static function store()
     {
         return auth()->user()->hasPermission('create_user');
     }
-    public function edit()
+    public static function edit()
     {
         return auth()->user()->hasPermission('edit_user');
     }
 
-    public function update()
+    public static function update()
     {
         return auth()->user()->hasPermission('edit_user');
     }
 
-    public function destroy()
+    public static function destroy()
     {
         return auth()->user()->hasPermission('delete_user');
     }
