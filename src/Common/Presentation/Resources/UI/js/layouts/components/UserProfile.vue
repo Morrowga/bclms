5cc30d7842dec2b338dd38070d467dcf2096aa4a
Aung-Myint-Myat-23711 <112838755+Aung-Myint-Myat-23711@users.noreply.github.com>
2023-08-11 15:52:36 +0630

    add-component

--- src/Common/Presentation/Resources/UI/js/layouts/components/UserProfile.vue
+++ src/Common/Presentation/Resources/UI/js/layouts/components/UserProfile.vue
@@ -8,118 +8,168 @@ const ability = useAppAbility();
 const userData = computed(() => usePage().props.auth);
 
 const logout = () => {
-  const PREFIX = localStorage.getItem("tenant") !="" ? `/${localStorage.getItem("tenant") }` : "";
-  localStorage.removeItem("menu_title");
-  router.post(`${PREFIX}/logout`);
+    const PREFIX =
+        localStorage.getItem("tenant") != ""
+            ? `/${localStorage.getItem("tenant")}`
+            : "";
+    localStorage.removeItem("menu_title");
+    router.post(`${PREFIX}/logout`);
 };
 </script>
 
 <template>
-  <VBadge
-    dot
-    location="bottom right"
-    offset-x="3"
-    offset-y="3"
-    color="success"
-    bordered
-  >
-    <VAvatar class="cursor-pointer" color="primary" variant="tonal">
-      <VImg
-        v-if="userData?.data && userData?.data?.image"
-        :src="userData?.data?.image"
-      />
-      <VIcon v-else icon="mdi-account-outline" />
+    <VBadge
+        dot
+        location="bottom right"
+        offset-x="3"
+        offset-y="3"
+        color="success"
+        bordered
+    >
+        <VAvatar class="cursor-pointer" color="primary" variant="tonal">
+            <VImg
+                v-if="userData?.data && userData?.data?.image"
+                :src="userData?.data?.image"
+            />
+            <VIcon v-else icon="mdi-account-outline" />
 
-      <!-- SECTION Menu -->
-      <VMenu activator="parent" width="230" location="bottom end" offset="14px">
-        <VList>
-          <!-- 👉 User Avatar & Name -->
-          <VListItem>
-            <template #prepend>
-              <VListItemAction start>
-                <VBadge
-                  dot
-                  location="bottom right"
-                  offset-x="3"
-                  offset-y="3"
-                  color="success"
-                >
-                  <VAvatar color="primary" variant="tonal">
-                    <VImg
-                      v-if="userData?.data && userData?.data?.image"
-                      :src="userData?.data?.image"
-                    />
-                    <VIcon v-else icon="mdi-account-outline" />
-                  </VAvatar>
-                </VBadge>
-              </VListItemAction>
-            </template>
+            <!-- SECTION Menu -->
 
-            <VListItemTitle class="font-weight-semibold">
-              {{ userData?.data?.name }}
-            </VListItemTitle>
-            <VListItemSubtitle>{{
-              userData?.data?.roles?.[0]?.name
-            }}</VListItemSubtitle>
-          </VListItem>
+            <VMenu activator="parent" width="230" offset="14px" class="my-menu">
+                <VList>
+                    <!-- 👉 User Avatar & Name -->
+                    <VListItem>
+                        <template #prepend>
+                            <VListItemAction start>
+                                <VBadge
+                                    dot
+                                    location="bottom right"
+                                    offset-x="3"
+                                    offset-y="3"
+                                    color="success"
+                                >
+                                    <VAvatar color="primary" variant="tonal">
+                                        <VImg
+                                            v-if="
+                                                userData?.data &&
+                                                userData?.data?.image
+                                            "
+                                            :src="userData?.data?.image"
+                                        />
+                                        <VIcon
+                                            v-else
+                                            icon="mdi-account-outline"
+                                        />
+                                    </VAvatar>
+                                </VBadge>
+                            </VListItemAction>
+                        </template>
 
-          <VDivider class="my-2" />
+                        <VListItemTitle class="font-weight-semibold">
+                            {{ userData?.data?.name }}
+                        </VListItemTitle>
+                        <VListItemSubtitle>{{
+                            userData?.data?.roles?.[0]?.name
+                        }}</VListItemSubtitle>
+                    </VListItem>
 
-          <!-- 👉 Profile -->
-          <VListItem @click="() => router.get('userprofile')">
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-account-outline" size="22" />
-            </template>
+                    <VDivider class="my-2" />
 
-            <VListItemTitle>Profile</VListItemTitle>
-          </VListItem>
+                    <!-- 👉 Profile -->
+                    <VListItem @click="() => router.get('userprofile')">
+                        <template #prepend>
+                            <VIcon
+                                class="me-2"
+                                icon="mdi-account-outline"
+                                size="22"
+                            />
+                        </template>
 
-          <!-- 👉 Settings -->
-          <VListItem
-            :to="{
-              name: 'pages-account-settings-tab',
-              params: { tab: 'account' },
-            }"
-          >
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-cog-outline" size="22" />
-            </template>
+                        <VListItemTitle>Profile</VListItemTitle>
+                    </VListItem>
 
-            <VListItemTitle>Settings</VListItemTitle>
-          </VListItem>
+                    <!-- 👉 Settings -->
+                    <VListItem
+                        :to="{
+                            name: 'pages-account-settings-tab',
+                            params: { tab: 'account' },
+                        }"
+                    >
+                        <template #prepend>
+                            <VIcon
+                                class="me-2"
+                                icon="mdi-cog-outline"
+                                size="22"
+                            />
+                        </template>
 
-          <!-- 👉 Pricing -->
-          <VListItem :to="{ name: 'pages-pricing' }">
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-currency-usd" size="22" />
-            </template>
+                        <VListItemTitle>Settings</VListItemTitle>
+                    </VListItem>
 
-            <VListItemTitle>Pricing</VListItemTitle>
-          </VListItem>
+                    <!-- 👉 Pricing -->
+                    <VListItem :to="{ name: 'pages-pricing' }">
+                        <template #prepend>
+                            <VIcon
+                                class="me-2"
+                                icon="mdi-currency-usd"
+                                size="22"
+                            />
+                        </template>
 
-          <!-- 👉 FAQ -->
-          <VListItem :to="{ name: 'pages-faq' }">
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-help-circle-outline" size="22" />
-            </template>
+                        <VListItemTitle>Pricing</VListItemTitle>
+                    </VListItem>
 
-            <VListItemTitle>FAQ</VListItemTitle>
-          </VListItem>
+                    <!-- 👉 FAQ -->
+                    <VListItem :to="{ name: 'pages-faq' }">
+                        <template #prepend>
+                            <VIcon
+                                class="me-2"
+                                icon="mdi-help-circle-outline"
+                                size="22"
+                            />
+                        </template>
 
-          <!-- Divider -->
-          <VDivider class="my-2" />
+                        <VListItemTitle>FAQ</VListItemTitle>
+                    </VListItem>
 
-          <!-- 👉 Logout -->
-          <VListItem link @click="logout">
-            <template #prepend>
-              <VIcon class="me-2" icon="mdi-logout" size="22" />
-            </template>
+                    <!-- Divider -->
+                    <VDivider class="my-2" />
 
-            <VListItemTitle>Logout</VListItemTitle>
-          </VListItem>
-        </VList>
-      </VMenu>
-      <!-- !SECTION -->
-    </VAvatar>
-  </VBadge>
+                    <!-- 👉 Logout -->
+                    <VListItem link @click="logout">
+                        <template #prepend>
+                            <VIcon class="me-2" icon="mdi-logout" size="22" />
+                        </template>
+
+                        <VListItemTitle>Logout</VListItemTitle>
+                    </VListItem>
+                </VList>
+            </VMenu>
+
+            <!-- !SECTION -->
+        </VAvatar>
+    </VBadge>
 </template>
+
+<style scoped>
+/* .my-menu {
+    margin-top: 40px;
+    contain: initial;
+    overflow: visible;
+}
+.my-menu::before {
+    position: fixed;
+    content: "";
+    top: 0;
+    right: 10px;
+    transform: translateY(-100%);
+    width: 10px;
+    height: 13px;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 13px solid rgb(var(--v-theme-surface));
+} */
+.menu-profile {
+    position: relative;
+}
+</style>
---
+++ src/Common/Presentation/Resources/components/NavItemDropDownMenu/NavItemDropDownMenu.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { router } from "@inertiajs/core";
+import { usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+const props = defineProps({
+    item: {
+        type: null,
+        required: true,
+    },
+    childrenAtEnd: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    isSubItem: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+});
+
+defineOptions({ name: "HorizontalNavGroup" });
+
+const auth = computed(() => usePage().props.auth);
+
+const isGroupActive = ref(false);
+let isLinkActive = (currentRoute) => {
+    return route()?.current()?.includes(currentRoute);
+};
+let isParentActive = (routeList) => {
+    return routeList?.find((item) =>
+        route()?.current()?.includes(item.route_name)
+    );
+};
+let goLink = (item) => {
+    if (item?.isNativeLink) {
+        window.location.href = item.url;
+    } else {
+        router.get(item.url);
+    }
+};
+
+let hiddenByPermission = (item) => {
+    !auth?.value?.data?.permissions?.includes(item?.access_module) &&
+    item?.access_module != "access_dashboard"
+        ? true
+        : false;
+};
+</script>
+
+<template>
+    <div class="text-center">
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+                <v-list-item
+                    v-bind="props"
+                    :prepend-icon="item.icon.icon"
+                    append-icon="mdi-chevron-down"
+                    :title="item.title"
+                    class="mx-2 text-none"
+                    :class="isParentActive(item.children) ? '' : ''"
+                    :color="
+                        isParentActive(item.children) ? '#4066E4' : '#282828'
+                    "
+                    :hidden="hiddenByPermission(item)"
+                >
+                </v-list-item>
+            </template>
+
+            <v-list density="compact">
+                <v-list-item
+                    v-for="(sitem, sindex) in item.children"
+                    :key="sindex"
+                    :value="sitem"
+                    @click="goLink(sitem)"
+                    :variant="isLinkActive(sitem.route_name) ? 'tonal' : 'text'"
+                    :hidden="hiddenByPermission(sitem)"
+                >
+                    <template v-slot:prepend>
+                        <v-icon icon="mdi-circle-small"></v-icon>
+                    </template>
+                    <v-list-item-title>{{ sitem.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+</template>
+
+<style lang="scss">
+.active-list {
+    background-color: #ededff !important;
+    color: #666cff !important;
+}
+
+.v-list-group--prepend {
+    --parent-padding: var(--indent-padding) !important;
+}
+.layout-horizontal-nav {
+    .nav-group {
+        .nav-group-label {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+        }
+
+        .popper-content {
+            z-index: 1;
+
+            > div {
+                overflow-x: hidden;
+                overflow-y: auto;
+            }
+        }
+    }
+}
+.bg-primary {
+    background-color: red !important;
+}
+</style>
