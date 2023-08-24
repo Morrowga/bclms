5b0c6ea3d7a9e07582ae5b6b59e2f09c89a98632
zawzawwin-dev <110709031+zawzawwin-dev@users.noreply.github.com>
2023-08-24 17:11:07 +0630

    prime vue setup for file upload

--- src/BlendedConcept/Security/Presentation/Resources/Users/components/ImportUserTypeData.vue
+++ src/BlendedConcept/Security/Presentation/Resources/Users/components/ImportUserTypeData.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue"
 const isDialogVisible = ref(false)
-
+import FileUpload from 'primevue/fileupload';
 const items = [
     'Blended Concepted',
     'Blended Center',
@@ -16,7 +16,7 @@ const items = [
         <!-- Activator -->
         <template #activator="{ props }">
             <VBtn v-bind="props" color="primary" variant="flat" width="164px">
-                    <span class="text-white">Next</span>
+                <span class="text-white">Next</span>
             </VBtn>
         </template>
 
@@ -24,14 +24,21 @@ const items = [
         <VCard class="d-flex justify-center">
             <VCardTitle class="d-flex justify-content-center align-center  gap-16">
                 <h4 class="tiggie-title">
-                    Batch Import Users
+                    Upload Teachers
                 </h4>
                 <VIcon icon="mdi-file-download" color="primary" size="30px" />
             </VCardTitle>
             <VCardText>
-                <VLabel class="tiggie-label required">Organization</VLabel>
-                <VSelect class="mt-3" density="compact" :items="items"
-                    placeholder="Select organization to import users for" />
+                <p class="tiggie-p"> <span class="text-primary pr-2">2</span>teachers remaining</p>
+            </VCardText>
+            <VCardText>
+                <FileUpload name="demo[]" url="./upload.php" @upload="onAdvancedUpload($event)" :multiple="true"
+                    accept="image/*" :maxFileSize="1000000">
+                    <template #empty>
+                        <p>Drag and drop files to here to upload.</p>
+                    </template>
+                </FileUpload>
+
             </VCardText>
 
             <VCardActions class="d-flex justify-center gap-5 ml-5 mt-16">
@@ -45,3 +52,7 @@ const items = [
         </VCard>
     </VDialog>
 </template>
+
+<style scoped>
+@import"primevue/resources/themes/lara-light-indigo/theme.css";
+</style>
