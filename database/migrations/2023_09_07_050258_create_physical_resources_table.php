b5fa27325a0dbfbbf52423d92a2c7d85240527ab
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-09-07 13:42:02 +0630

    table update

---
+++ database/migrations/2023_09_07_050258_create_physical_resources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('physical_resources', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('storybook_id')->references('id')->on('storybooks');
+            $table->string('file_src')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('physical_resources');
+    }
+};
