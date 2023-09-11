2e299420b4ba904c75a09a4c116091df3e3bacb0
zawzawwin-dev <110709031+zawzawwin-dev@users.noreply.github.com>
2023-09-11 17:38:18 +0630

    ddd

--- src/BlendedConcept/Security/Infrastructure/EloquentModels/UserEloquentModel.php
+++ src/BlendedConcept/Security/Infrastructure/EloquentModels/UserEloquentModel.php
@@ -9,7 +9,6 @@ use Hash;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\MediaLibrary\HasMedia;
