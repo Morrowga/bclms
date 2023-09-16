d846e3c2e5ee620befd3eb304d35c36dc529f3a3
zaw-dev <110709031+zawzawwin-dev@users.noreply.github.com>
2023-09-17 01:23:57 +0630

    fixed nwewind

--- src/BlendedConcept/StoryBook/Application/DTO/PathwayData.php
+++ src/BlendedConcept/StoryBook/Application/DTO/PathwayData.php
@@ -16,15 +16,15 @@ class PathwayData
     ) {
     }
 
-    public static function fromRequest(Request $request, $pathway_id): PathwayData
+    public static function fromRequest(Request $request, $reward_id): PathwayData
     {
         return new self(
-            id : $pathway_id,
-            name : $this->name,
-            description : $this->description,
-            num_gold_coins : $this->num_gold_coins,
-            num_silver_coins : $this->num_silver_coins,
-            need_complete_in_order : $this->need_complete_in_order,
+            id : $reward_id,
+            file_src : $request->file_src,
+            gold_coins : $request->gold_coins,
+            silver_coins : $request->silver_coins,
+            status : $request->status,
+            rarity : $request->rarity,
         );
     }
 
