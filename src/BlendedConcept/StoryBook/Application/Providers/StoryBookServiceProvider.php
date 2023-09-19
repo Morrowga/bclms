5cc599ab19b7b73080f09529ca01e5a53685d0d4
zaw-dev <110709031+zawzawwin-dev@users.noreply.github.com>
2023-09-19 09:29:25 +0630

    Update StoryBookServiceProvider.php

--- src/BlendedConcept/StoryBook/Application/Providers/StoryBookServiceProvider.php
+++ src/BlendedConcept/StoryBook/Application/Providers/StoryBookServiceProvider.php
@@ -5,8 +5,8 @@ namespace Src\BlendedConcept\StoryBook\Application\Providers;
 use Illuminate\Support\ServiceProvider;
 use Src\BlendedConcept\StoryBook\Application\Repositories\Eloquent\RewardRepository;
 use Src\BlendedConcept\StoryBook\Domain\Repositories\RewaredRepositoryInterface;
-use Src\BlendedConcept\StoryBook\Domain\Repositories\StoryBookRepositoryInterface;
-use Src\BlendedConcept\StoryBook\Application\Repositories\Eloquent\StoryBookRepository;
+use Src\BlendedConcept\StoryBook\Application\Repositories\Eloquent\PathwayRepository;
+use Src\BlendedConcept\StoryBook\Domain\Repositories\PathwayRepositoryInterface;
 
 class StoryBookServiceProvider extends ServiceProvider
 {
@@ -18,8 +18,8 @@ class StoryBookServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(
-            StoryBookRepositoryInterface::class,
-            StoryBookRepository::class
+            PathwayRepositoryInterface::class,
+            PathwayRepository::class
         );
     }
 }
