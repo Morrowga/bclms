6b8a36fb4660a26e659a50be522155683a3ce894
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-09-23 23:59:31 +0630

    fix survey test and profiling

--- tests/Feature/UserExperienceSurveyTest.php
+++ tests/Feature/UserExperienceSurveyTest.php
@@ -117,7 +117,6 @@ test('update user experience survey with bcstaff roles', function () {
     // Start a database transaction for the test
     $this->assertTrue(Auth::check());
 
-    // Create a survey that you intend to update
     $questions = [
         [
             "id" => '1',
@@ -137,7 +136,7 @@ test('update user experience survey with bcstaff roles', function () {
         'end_date' => Carbon::now()->addDay(3),
         'required' => true,
         'repeat' => true,
-        'questions' => json_encode($questions),
+        'questions' => json_encode($questions)
     ]);
 
     $response->assertStatus(302);
@@ -146,19 +145,10 @@ test('update user experience survey with bcstaff roles', function () {
     $surveyId = 1; // Retrieve the survey ID as needed
 
     // Attempt to update the survey
-    $updateQuestions = [
-        [
-            "id" => '1',
-            "question_type" => 'MULTIPLE_RESPONSE',
-            "question" => 'That is question',
-            "options" => ['That is update options', 'That is update option 2', 'That is option 3']
-        ]
-    ];
 
     $updateResponse = $this->put("/userexperiencesurvey/{$surveyId}", [
         'title' => 'Updated Survey',
         'description' => 'Updated Survey Description',
-        'questions' => $updateQuestions,
         'type' => 'USERREXP',
         'user_type' => 'ORG_TEACHER',
         'appear_on' => 'BOOK_END',
@@ -176,6 +166,142 @@ test('update user experience survey with bcstaff roles', function () {
     $updateData->assertSessionHasErrors(['title', 'description', 'type', 'user_type', 'appear_on', 'start_date', 'end_date']);
 
     $response->assertRedirect('/userexperiencesurvey');
+});
+
+test('add new question in user experience survey with bcstaff roles', function () {
+    $this->assertTrue(Auth::check());
+
+    $questions = [
+        [
+            "id" => '1',
+            "question_type" => 'SINGLE_CHOICE',
+            "question" => 'That is question',
+            "options" => ['That is options', 'That is option 2', 'That is option 3']
+        ]
+    ];
+
+    // Create the initial user experience survey
+    $response = $this->post('/userexperiencesurvey', [
+        'title' => 'Example Survey',
+        'description' => 'Survey Description',
+        'type' => 'USERREXP',
+        'user_type' => 'B2C_USER',
+        'appear_on' => 'LOG_OUT',
+        'start_date' => Carbon::now(),
+        'end_date' => Carbon::now()->addDay(3),
+        'required' => true,
+        'repeat' => true,
+        'questions' => json_encode($questions),
+    ]);
+
+    $response->assertStatus(302);
+
+    $surveyId = 1;
+
+    // Create a new question for the survey
+    $questionResponse = $this->post('/questions', [
+        "survey_id" => $surveyId,
+        "question_type" => 'SINGLE_CHOICE',
+        "question" => 'That is question',
+        "options" => json_encode(['That is new options', 'That is new option 2', 'That new is option 3'])
+    ]);
+
+    $addQuestionData = $this->post("/questions", []);
+
+    $addQuestionData->assertSessionHasErrors(['survey_id', 'question_type', 'question', 'options']);
+
+    $questionResponse->assertStatus(302);
+    // $response->assertRedirect($questionResponse->headers->get('Location'));
+});
+
+
+test('update existing question in user experience survey with bcstaff roles', function () {
+    $this->assertTrue(Auth::check());
+
+    $questions = [
+        [
+            "id" => '1',
+            "question_type" => 'SINGLE_CHOICE',
+            "question" => 'That is question',
+            "options" => ['That is options', 'That is option 2', 'That is option 3']
+        ]
+    ];
+
+    $response = $this->post('/userexperiencesurvey', [
+        'title' => 'Example Survey',
+        'description' => 'Survey Description',
+        'type' => 'USERREXP',
+        'user_type' => 'B2C_USER',
+        'appear_on' => 'LOG_OUT',
+        'start_date' => Carbon::now(),
+        'end_date' => Carbon::now()->addDay(3),
+        'required' => true,
+        'repeat' => true,
+        'questions' => json_encode($questions),
+    ]);
+
+    $response->assertStatus(302);
+
+
+    // Define the route name and route parameters
+    $questionId =  1;
+    $surveyId =  1;
+
+    // Define the updated question data
+    $updatedQuestionData = [
+        "survey_id" => $surveyId,
+        "question_type" => 'MULTIPLE_RESPONSE',
+        "question" => 'That is update question',
+        "options" => json_encode(['That is update options', 'That is update option 2', 'That is option 3'])
+    ];
+
+    $questionResponse = $this->put("/questions/{$questionId}", $updatedQuestionData);
+
+    $updateQuestionData = $this->put("/questions/{$questionId}", []);
+
+    $updateQuestionData->assertSessionHasErrors(['survey_id', 'question_type', 'question', 'options']);
+
+    $questionResponse->assertStatus(302);
+});
+
+
+test('delete  the question in user experience survey with bcstaff roles', function () {
+    // Start a database transaction for the test
+    $this->assertTrue(Auth::check());
+
+    // Create a survey that you intend to delete
+    $questions = [
+        [
+            "id" => '1',
+            "question_type" => 'SINGLE_CHOICE',
+            "question" => 'That is question',
+            "options" => ['That is options', 'That is option 2', 'That is option 3']
+        ]
+    ];
+
+    $response = $this->post('/userexperiencesurvey', [
+        'title' => 'Survey to Delete',
+        'description' => 'Survey to Delete Description',
+        'type' => 'USERREXP',
+        'user_type' => 'B2C_USER',
+        'appear_on' => 'LOG_OUT',
+        'start_date' => Carbon::now(),
+        'end_date' => Carbon::now()->addDay(3),
+        'required' => true,
+        'repeat' => true,
+        'questions' => json_encode($questions),
+    ]);
+
+    $response->assertStatus(302);
+
+    // Extract the survey ID from the response or retrieve it from the database
+    $surveyId = 1; // Retrieve the survey ID as needed
+    $questionId = 1;
+
+    // Attempt to delete the survey
+    $deleteResponse = $this->delete("/questions/{$questionId}");
+
+    $deleteResponse->assertStatus(302);
 });
 
 test('delete user experience survey with bcstaff roles', function () {
@@ -215,3 +341,4 @@ test('delete user experience survey with bcstaff roles', function () {
 
     $deleteResponse->assertStatus(302);
 });
+
