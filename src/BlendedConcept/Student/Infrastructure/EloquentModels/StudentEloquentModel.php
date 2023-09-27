405786e1b69d24852c5ceb68b2b72d272d7a202e
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-09-28 00:04:27 +0630

    org student table remove

--- src/BlendedConcept/Student/Infrastructure/EloquentModels/StudentEloquentModel.php
+++ src/BlendedConcept/Student/Infrastructure/EloquentModels/StudentEloquentModel.php
@@ -82,7 +82,7 @@ class StudentEloquentModel extends Model implements HasMedia
 
     public function organisations()
     {
-        return $this->belongsToMany(OrganisationEloquentModel::class, 'organisation_students', 'student_id', 'organisation_id');
+        return $this->belongsToMany(OrganisationEloquentModel::class, 'organisation_id');
     }
 
     public function classrooms()
