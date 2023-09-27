abf571caa701fa06b6b8010cf79380b9c154a9fd
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-09-27 16:05:16 +0630

    sticker migration

---
+++ database/migrations/2023_09_27_093237_create_student_sticker_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_sticker', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->references('student_id')->on('students')->onDelete('cascade');
+            $table->foreignId('sticker_id')->references('id')->on('stickers')->onDelete('cascade');
+            $table->float('y_axis_position', 10,6)->nullable();
+            $table->float('x_axis_position', 10,6)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_sticker');
+    }
+};
