b36364667f2376dc43a189ad890fa01ac29c7ac0
David <devdavid23711@gmail.com>
2023-09-27 14:52:33 +0630

    add-tables

---
+++ database/migrations/2023_09_27_065400_create_teacher_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_students', function (Blueprint $table) {
+            $table->foreignId('teacher_id')->nullable()
+                ->references('teacher_id')
+                ->on('teachers')->onDelete('cascade');
+            $table->foreignId('student_id')->nullable()
+                ->references('student_id')
+                ->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_students');
+    }
+};
