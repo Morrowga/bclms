0fb595f0e704bf1121bca92b62e6a2384431585c
thihaeung <124340704+thihaeungg@users.noreply.github.com>
2023-10-05 12:32:44 +0630

    student auth

--- src/BlendedConcept/Student/Presentation/HTTP/routes.php
+++ src/BlendedConcept/Student/Presentation/HTTP/routes.php
@@ -17,6 +17,8 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::resource('/teacher_students', TeacherStudentController::class);
     Route::get('/teacher_student/kidmode/{user}', [TeacherStudentController::class, 'kidMode'])->name('teacher_students.kidmode');
+    Route::post('/teacher_student/exitmode/{user}', [TeacherStudentController::class, 'exitMode'])->name('teacher_students.exitmode');
+
     Route::get('/teacher_student/profiling_surveys', [ProfilingSurveyController::class, 'index'])->name('teacher_students.profiling_surveys');
     // Route::get('/view_students/show', [ViewStudentController::class, 'show'])->name('view_students.show');
     // Route::get('/view_students/create', [ViewStudentController::class, 'create'])->name('view_students.create');
--- src/Common/Presentation/Resources/components/Student/ExitMode.vue
+++ src/Common/Presentation/Resources/components/Student/ExitMode.vue
@@ -1,20 +1,36 @@
 <script setup>
 import { router } from "@inertiajs/core";
+import { useForm } from "@inertiajs/vue3";
+import {
+  emailValidator,
+  requiredValidator,
+  integerValidator,
+} from "@validators";
 
-let props = defineProps(["route", "count", "label"]);
+let props = defineProps(["route", "count", "teacher_id", "student_id"]);
 const isDialogVisible = ref(false);
 let passwordVisible = ref(false);
+
 const togglePasswordVisibility = () => {
     passwordVisible.value = !passwordVisible.value;
 };
+
+const form = useForm({
+    password: null,
+    student_id: props.student_id
+})
+
+
 const logout = () => {
-    // const PREFIX =
-    //     localStorage.getItem("tenant") != ""
-    //         ? `/${localStorage.getItem("tenant")}`
-    //         : "";
-    // localStorage.removeItem("menu_title");
-    router.post(`/logout`);
+    form.post(route("teacher_students.exitmode", props.teacher_id), {
+    onSuccess: () => {
+    },
+    onError: (error) => {
+    },
+  });
 };
+
+
 </script>
 <template #activator="{ props }">
     <div>
@@ -30,61 +46,67 @@ const logout = () => {
         <VDialog v-model="isDialogVisible" width="500">
             <!-- Activator -->
             <!-- Dialog Content -->
-            <VCard class="rolling-card">
-                <VCardText>
-                    <div class="text-center mt-15">
-                        <span class="password-label pppangram-bold"
-                            >Enter Password</span
-                        >
-                        <div class="passwordinput-container mt-3 mx-15">
-                            <VTextField
-                                v-bind:type="
-                                    passwordVisible ? 'text' : 'password'
-                                "
-                                class="mt-3 passwordinput mx-5"
-                                placeholder="Password"
-                            />
-                            <span
-                                class="toggle-password"
-                                v-on:click="togglePasswordVisibility"
+            <VForm @submit.prevent="logout()">
+                <VCard class="rolling-card">
+                    <VCardText>
+                        <div class="text-center mt-15">
+                            <span class="password-label pppangram-bold"
+                                >Enter Password</span
                             >
-                                <VIcon
-                                    :icon="
-                                        passwordVisible
-                                            ? 'mdi-eye-off-outline'
-                                            : 'mdi-eye-outline'
+                            <div class="passwordinput-container mt-3 mx-15">
+                                <VTextField
+                                    v-model="form.password"
+                                    v-bind:type="
+                                        passwordVisible ? 'text' : 'password'
                                     "
+                                    class="mt-3 passwordinput mx-5"
+                                    placeholder="Password"
+                                    :rules="[requiredValidator]"
+                                    :error-messages="form?.errors?.password"
                                 />
-                            </span>
+                                <span
+                                    class="toggle-password"
+                                    v-on:click="togglePasswordVisibility"
+                                >
+                                    <VIcon
+                                        :icon="
+                                            passwordVisible
+                                                ? 'mdi-eye-off-outline'
+                                                : 'mdi-eye-outline'
+                                        "
+                                    />
+                                </span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="d-flex justify-center mt-15">
-                        <v-btn
-                            @click="logout()"
-                            varient="flat"
-                            color="#D7F2F0"
-                            class="pppangram-bold mr-3 confirmbtn"
-                        >
-                            Confirm
-                        </v-btn>
-                        <v-btn
-                            @click="isDialogVisible = false"
-                            varient="flat"
-                            color="#BFC0C1"
-                            class="pppangram-bold ml-5 cancelbtn"
-                        >
-                            Cancel
-                        </v-btn>
-                    </div>
-                </VCardText>
-                <!-- <VCardActions>
-            <VSpacer />
-            <VBtn @click="isDialogVisible = false">
-            I accept
-            </VBtn>
-        </VCardActions> -->
-            </VCard>
+                        <div class="d-flex justify-center mt-15">
+                            <v-btn
+                                type="submit"
+                                varient="flat"
+                                color="#D7F2F0"
+                                class="pppangram-bold mr-3 confirmbtn"
+                            >
+                                Confirm
+                            </v-btn>
+                            <v-btn
+                                @click="isDialogVisible = false"
+                                varient="flat"
+                                color="#BFC0C1"
+                                class="pppangram-bold ml-5 cancelbtn"
+                            >
+                                Cancel
+                            </v-btn>
+                        </div>
+                    </VCardText>
+                    <!-- <VCardActions>
+                <VSpacer />
+                <VBtn @click="isDialogVisible = false">
+                I accept
+                </VBtn>
+            </VCardActions> -->
+                </VCard>
+            </VForm>
+
         </VDialog>
     </div>
 </template>
