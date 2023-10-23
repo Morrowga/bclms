013a08a487bdc27a8d13e55e96fecf5cf7b39d10
David <devdavid23711@gmail.com>
2023-10-23 16:27:02 +0630

    fix-plan-2

--- resources/views/register_email.blade.php
+++ resources/views/register_email.blade.php
@@ -1,36 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tiggie Kids</title>
 </head>
+
 <body>
-    # Welcome to Tiggie Kids - Verify Your Account
+    Subject: Welcome to Tiggie Kids - Verify Your Account
+    <br />
+    <br />
 
     Dear {{ $name }},
+    <br />
+    <br />
 
-    Thank you for choosing Tiggie Kids, your trusted partner in online learning management and interactive storytelling! We're thrilled to have you as part of our educational community.
+    Thank you for choosing Tiggie Kids, your trusted partner in online learning management <br /> and interactive
+    storytelling! We're thrilled to have you as part of our educational <br />community.
 
-    Before you get started, we want to ensure the security of your account. Please take a moment to verify your email address by clicking the link below:
+    Before you get started, we want to ensure the security of your account. Please take a <br /> moment to verify your
+    email
+    address by clicking the link below:<br />
     {{ $verificationLink }}
 
-    By verifying your email, you'll have full access to Tiggie Kids' powerful features designed to enhance the learning experience for your students.
+    By verifying your email, you'll have full access to Tiggie Kids' powerful features designed to <br /> enhance the
+    learning experience for your students. <br />
 
-    Here's a glimpse of what you can do with Tiggie Kids:
-    Interactive Storybooks: Create and use interactive features in our storybooks to make learning engaging and fun for your students. You can customize lessons to suit your little one's needs.
-    Manage Educational Resources: Upload and use your own educational media resources to be used in the interactive storybook for a personal touch.
+    Here's a glimpse of what you can do with Tiggie Kids:<br />
+    <ol type="1">
+        <li> <strong>Interactive Storybooks:</strong> Create and use interactive features in our storybooks to
+            make<br /> learning engaging and fun for your students. You can customize lessons to suit your<br /> little
+            one's needs.</li>
+        <li> <strong>Educational Resources:</strong> Upload and use your own educational media <br /> resources to be
+            used in the interactive storybook for a personal touch.</li>
+    </ol>
 
-    If you ever encounter any issues or if you have any questions, please don't hesitate to contact our dedicated support staff at {{ $email }} and {{$contact_number}}. We're here to assist you in every step of your educational journey with Tiggie Kids.
 
-    Thank you for choosing Tiggie Kids as your partner in education. We look forward to helping you provide the best learning experience for your students.
+
+
+    If you ever encounter any issues or if you have any questions, please don't hesitate to <br />contact our dedicated
+    support staff at {{ $email }} and {{ $contact_number }}. We're <br />here to assist you in every step of your
+    educational journey with Tiggie Kids.
+
+    Thank you for choosing Tiggie Kids as your partner in education. We look forward to helping <br /> you provide the
+    best
+    learning experience for your students.
 
     Happy teaching!
+    <br />
+    <br />
 
     Sincerely,
+    <br />
     [BC Staff Name]
+    <br />
     Blended Concept Admin
 </body>
-</html>
 
+</html>
