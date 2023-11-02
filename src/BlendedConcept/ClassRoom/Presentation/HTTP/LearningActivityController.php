79c0c4fe7df0fada42afd0dda0352df476bec1bc
Food Burma <foodburma.dev@gmail.com>
2023-11-02 17:01:37 +0630

    fix to show h5p result for storybook assigned student

--- src/BlendedConcept/ClassRoom/Presentation/HTTP/LearningActivityController.php
+++ src/BlendedConcept/ClassRoom/Presentation/HTTP/LearningActivityController.php
@@ -5,6 +5,7 @@ namespace Src\BlendedConcept\ClassRoom\Presentation\HTTP;
 use Devleaptech\LaravelH5p\Eloquents\H5pContent;
 use Inertia\Inertia;
 use Src\BlendedConcept\Organisation\Infrastructure\EloquentModels\StudentEloquentModel;
+use Src\BlendedConcept\StoryBook\Infrastructure\EloquentModels\StoryBookAssignmentEloquentModel;
 use Src\BlendedConcept\StoryBook\Infrastructure\EloquentModels\StoryBookEloquentModel;
 use Src\BlendedConcept\StoryBook\Infrastructure\EloquentModels\StoryBookVersionEloquentModel;
 
@@ -12,11 +13,11 @@ class LearningActivityController
 {
     public function index(StoryBookEloquentModel $storybook, StudentEloquentModel $student)
     {
-        // $verionIds = $storybook->storybook_versions->pluck('h5p_id'); // get h5p_id from storybook version
-        $teacher_ids = $student->teachers()->pluck('teachers.teacher_id');
-        $parent_ids = $student->parent()->pluck('parent_id');
-        $verionIds = StoryBookVersionEloquentModel::where('storybook_id', $storybook->id)->whereIn('teacher_id', $teacher_ids)->orWhereIn('parent_id', $parent_ids)->pluck('h5p_id');
-
+        $storybook_version_ids = StoryBookAssignmentEloquentModel::where('student_id', $student->student_id)->pluck('storybook_version_id');
+        $verionIds = $storybook->storybook_versions->whereIn('id', $storybook_version_ids)->pluck('h5p_id'); // get h5p_id from storybook version
+        // $teacher_ids = $student->teachers()->pluck('teachers.teacher_id');
+        // $parent_ids = $student->parent()->pluck('parent_id');
+        // $verionIds = StoryBookVersionEloquentModel::where('storybook_id', $storybook->id)->whereIn('teacher_id', $teacher_ids)->orWhereIn('parent_id', $parent_ids)->pluck('h5p_id');
         $h5pContents = H5pContent::with('eloquent_user')->whereIn('id', $verionIds)->get(); // get all h5p content realated with storybook versions
 
         return Inertia::render(config('route.learning_activities.index'), [
