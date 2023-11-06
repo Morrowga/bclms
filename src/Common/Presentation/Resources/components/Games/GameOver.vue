da2b51b909ccb6d045e66b3e3a3540814618681f
David <devdavid23711@gmail.com>
2023-11-06 15:52:05 +0630

    fix-book-index-2

--- src/Common/Presentation/Resources/components/Games/GameOver.vue
+++ src/Common/Presentation/Resources/components/Games/GameOver.vue
@@ -2,58 +2,60 @@
 import { router } from "@inertiajs/core";
 
 let props = defineProps(["route", "count", "iframeSrc"]);
-const isDialogVisible = ref(false)
+const isDialogVisible = ref(false);
 </script>
 <template #activator="{ props }">
     <div class="d-flex justify-center">
         <iframe
-        class="videoplayer"
-        :src="iframeSrc"
-        frameborder="0"
-        allowfullscreen
+            class="videoplayer"
+            :src="iframeSrc"
+            frameborder="0"
+            allowfullscreen
         ></iframe>
         <!-- <img :src="image" class="videoplayer" @click="isDialogVisible = true" alt=""> -->
     </div>
-    <VDialog
-        v-model="isDialogVisible"
-        width="700"
-    >
+    <VDialog v-model="isDialogVisible" width="700">
         <VCard class="gameover-card">
-        <VCardText>
+            <VCardText>
                 <div class="d-flex justify-center text-center">
-                    <span class="ruddy-bold gameover-text ruddy-bold">congratulations, <strong class="ruddy-bold">100 Stars</strong> has been  <br> added to your account!</span>
+                    <span class="ruddy-bold gameover-text ruddy-bold"
+                        >congratulations,
+                        <strong class="ruddy-bold">100 Stars</strong> has been
+                        <br />
+                        added to your account!</span
+                    >
                 </div>
-        </VCardText>
+            </VCardText>
         </VCard>
     </VDialog>
 </template>
 <style scoped>
-.gameover-card{
+.gameover-card {
     /* height: 400px; */
-    background: url('/images/Game Pop Up.png') no-repeat;
+    background: url("/images/Game Pop Up.png") no-repeat;
     background-size: cover;
 }
 
-.videoplayer{
+.videoplayer {
     width: 100%;
-    height: 100vh;
+    height: 120vh;
 }
 
-.gameoverimg{
+.gameoverimg {
     object-fit: cover !important;
 }
 
 .gameover-text > strong {
-    color: #FF6262 !important;
+    color: #ff6262 !important;
     font-size: 30px !important;
     font-style: normal !important;
     font-weight: 700 !important;
     text-transform: capitalize !important;
 }
 
-.gameover-text{
+.gameover-text {
     margin-top: 50% !important;
-    color: var(--white, #FFF);
+    color: var(--white, #fff);
     font-size: 30px !important;
     font-style: normal !important;
     font-weight: 700 !important;
