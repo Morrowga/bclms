ad3011976aedf84328331474d0d714ed51b77669
David <devdavid23711@gmail.com>
2023-11-10 10:22:10 +0630

    fix-menu

--- src/Common/Presentation/Resources/UI/js/@layouts/components/VerticalNavLink.vue
+++ src/Common/Presentation/Resources/UI/js/@layouts/components/VerticalNavLink.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-list-item
-    :prepend-icon="item.icon.icon"
-    :title="item.title"
-    :value="item.title"
-    :class="isLinkActive(item.route_name) ? 'bg-primary' : ''"
-    :color="isLinkActive(item.route_name) ? '#fff' : ''"
-    @click="goLink(item.url)"
-    :hidden="
-      !auth?.data?.permissions?.includes(item?.access_module) &&
-      item?.access_module != 'access_dashboard'
-        ? true
-        : false
-    "
-  ></v-list-item>
+    <v-list-item
+        v-if="
+            !auth?.data?.permissions?.includes(item?.access_module) &&
+            item?.access_module != 'access_dashboard'
+                ? false
+                : true
+        "
+        :prepend-icon="item.icon.icon"
+        :title="item.title"
+        :value="item.title"
+        :class="isLinkActive(item.route_name) ? 'bg-primary' : ''"
+        :color="isLinkActive(item.route_name) ? '#fff' : ''"
+        @click="goLink(item.url)"
+    ></v-list-item>
 </template>
 <script setup>
 import { router } from "@inertiajs/core";
@@ -20,10 +20,10 @@ import { usePage } from "@inertiajs/vue3";
 const auth = computed(() => usePage().props.auth);
 defineProps(["item"]);
 let isLinkActive = (currentRoute) => {
-  return route()?.current()?.includes(currentRoute);
+    return route()?.current()?.includes(currentRoute);
 };
 let goLink = (url) => {
-  localStorage.removeItem("menu_title");
-  router.get(url);
+    localStorage.removeItem("menu_title");
+    router.get(url);
 };
 </script>
