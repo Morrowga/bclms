f50d7bc6808e4362dd54bd2de71738090f468257
David <devdavid23711@gmail.com>
2023-11-14 09:53:17 +0630

    fix-permission

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -21,10 +21,10 @@ class PermissionRoleTableSeeder extends Seeder
             ['name' => 'BC Staff', 'guard_name' => 'web'],
             ['name' => 'Teacher', 'guard_name' => 'web'],
             ['name' => 'Organisation Admin', 'guard_name' => 'web'],
-            ['name' => 'Student', 'guard_name' => 'web'],
-            ['name' => 'B2B Parent', 'guard_name' => 'web'],
-            ['name' => 'B2C Parent', 'guard_name' => 'web'],
-            ['name' => 'Both Parent', 'guard_name' => 'web'],
+            ['name' => 'Student', 'guard_name' => 'web']
+            // ['name' => 'B2B Parent', 'guard_name' => 'web'],
+            // ['name' => 'B2C Parent', 'guard_name' => 'web'],
+            // ['name' => 'Both Parent', 'guard_name' => 'web'],
         ];
         foreach ($datas as $data) {
             // $role = RoleEloquentModel::create($data);
@@ -320,110 +320,114 @@ class PermissionRoleTableSeeder extends Seeder
                     ]
                 )->pluck('id');
                 $role->permissions()->sync($permission);
-            } elseif ($data['name'] == 'B2B Parent') {
-                $role = RoleEloquentModel::create($data);
-                $permission = PermissionEloquentModel::whereIn(
-                    'name',
-                    [
-                        'access_reports',
-                        'access_exportData',
-                        'access_techSupport',
-                        'access_viewStudents',
-                        'access_teacherStudent',
-                        'show_teacherStudent',
-                        'create_teacherStudent',
-                        'edit_teacherStudent',
-                        'update_teacherStudent',
-                        'delete_teacherStudent',
-                        'create_techSupport',
-                        // 'access_playlist',
-                        'show_playlist',
-                        'access_teacherStorybook',
-                        'access_teacherBook',
-                        'show_teacherBook',
-                    ]
-                )->pluck('id');
-                $role->permissions()->sync($permission);
-            } elseif ($data['name'] == 'B2C Parent') {
-                $role = RoleEloquentModel::create($data);
-                $permission = PermissionEloquentModel::whereIn(
-                    'name',
-                    [
-                        // 'access_plan',
-                        'access_teacherStorybook',
-                        // 'access_resources',
-                        'access_viewStudents',
-                        'access_reports',
-                        'access_exportData',
-                        // 'access_playlist',
-                        'show_playlist',
-                        'create_playlist',
-                        'edit_playlist',
-                        'delete_playlist',
-                        'access_teacherBook',
-                        'create_teacherBook',
-                        'edit_teacherBook',
-                        'show_teacherBook',
-                        'delete_teacherBook',
-                        'access_assignStudent',
-                        'access_createVersion',
-                        'access_teacherStudent',
-                        'show_teacherStudent',
-                        'edit_teacherStudent',
-                        'create_teacherStudent',
-                        'update_teacherStudent',
-                        'delete_teacherStudent',
-                        'access_techSupport',
-                        // 'access_resources',
-                        'show_resources',
-                        'create_resources',
-                        'edit_resources',
-                        'delete_resources',
-                        'create_techSupport',
-                        'access_gameAssign'
-                    ]
-                )->pluck('id');
-                $role->permissions()->sync($permission);
-            } elseif ($data['name'] == 'Both Parent') {
-                $role = RoleEloquentModel::create($data);
-                $permission = PermissionEloquentModel::whereIn(
-                    'name',
-                    [
-                        // 'access_plan',
-                        'access_teacherStorybook',
-                        // 'access_resources',
-                        'access_viewStudents',
-                        'access_reports',
-                        'access_exportData',
-                        // 'access_playlist',
-                        'show_playlist',
-                        'create_playlist',
-                        'edit_playlist',
-                        'delete_playlist',
-                        'access_teacherBook',
-                        'create_teacherBook',
-                        'edit_teacherBook',
-                        'show_teacherBook',
-                        'delete_teacherBook',
-                        'access_assignStudent',
-                        'access_createVersion',
-                        'access_teacherStudent',
-                        'show_teacherStudent',
-                        'edit_teacherStudent',
-                        'create_teacherStudent',
-                        'update_teacherStudent',
-                        'delete_teacherStudent',
-                        'access_techSupport',
-                        // 'access_resources',
-                        'show_resources',
-                        'create_resources',
-                        'edit_resources',
-                        'delete_resources',
-                        'create_techSupport'
-                    ]
-                )->pluck('id');
-                $role->permissions()->sync($permission);
             }
+
+            // elseif ($data['name'] == 'B2B Parent') {
+            //     $role = RoleEloquentModel::create($data);
+            //     $permission = PermissionEloquentModel::whereIn(
+            //         'name',
+            //         [
+            //             'access_reports',
+            //             'access_exportData',
+            //             'access_techSupport',
+            //             'access_viewStudents',
+            //             'access_teacherStudent',
+            //             'show_teacherStudent',
+            //             'create_teacherStudent',
+            //             'edit_teacherStudent',
+            //             'update_teacherStudent',
+            //             'delete_teacherStudent',
+            //             'create_techSupport',
+            //             // 'access_playlist',
+            //             'show_playlist',
+            //             'access_teacherStorybook',
+            //             'access_teacherBook',
+            //             'show_teacherBook',
+            //         ]
+            //     )->pluck('id');
+            //     $role->permissions()->sync($permission);
+            // }
+            // elseif ($data['name'] == 'B2C Parent') {
+            //     $role = RoleEloquentModel::create($data);
+            //     $permission = PermissionEloquentModel::whereIn(
+            //         'name',
+            //         [
+            //             // 'access_plan',
+            //             'access_teacherStorybook',
+            //             // 'access_resources',
+            //             'access_viewStudents',
+            //             'access_reports',
+            //             'access_exportData',
+            //             // 'access_playlist',
+            //             'show_playlist',
+            //             'create_playlist',
+            //             'edit_playlist',
+            //             'delete_playlist',
+            //             'access_teacherBook',
+            //             'create_teacherBook',
+            //             'edit_teacherBook',
+            //             'show_teacherBook',
+            //             'delete_teacherBook',
+            //             'access_assignStudent',
+            //             'access_createVersion',
+            //             'access_teacherStudent',
+            //             'show_teacherStudent',
+            //             'edit_teacherStudent',
+            //             'create_teacherStudent',
+            //             'update_teacherStudent',
+            //             'delete_teacherStudent',
+            //             'access_techSupport',
+            //             // 'access_resources',
+            //             'show_resources',
+            //             'create_resources',
+            //             'edit_resources',
+            //             'delete_resources',
+            //             'create_techSupport',
+            //             'access_gameAssign'
+            //         ]
+            //     )->pluck('id');
+            //     $role->permissions()->sync($permission);
+            // } 
+            // elseif ($data['name'] == 'Both Parent') {
+            //     $role = RoleEloquentModel::create($data);
+            //     $permission = PermissionEloquentModel::whereIn(
+            //         'name',
+            //         [
+            //             // 'access_plan',
+            //             'access_teacherStorybook',
+            //             // 'access_resources',
+            //             'access_viewStudents',
+            //             'access_reports',
+            //             'access_exportData',
+            //             // 'access_playlist',
+            //             'show_playlist',
+            //             'create_playlist',
+            //             'edit_playlist',
+            //             'delete_playlist',
+            //             'access_teacherBook',
+            //             'create_teacherBook',
+            //             'edit_teacherBook',
+            //             'show_teacherBook',
+            //             'delete_teacherBook',
+            //             'access_assignStudent',
+            //             'access_createVersion',
+            //             'access_teacherStudent',
+            //             'show_teacherStudent',
+            //             'edit_teacherStudent',
+            //             'create_teacherStudent',
+            //             'update_teacherStudent',
+            //             'delete_teacherStudent',
+            //             'access_techSupport',
+            //             // 'access_resources',
+            //             'show_resources',
+            //             'create_resources',
+            //             'edit_resources',
+            //             'delete_resources',
+            //             'create_techSupport'
+            //         ]
+            //     )->pluck('id');
+            //     $role->permissions()->sync($permission);
+            // }
         }
     }
 }
