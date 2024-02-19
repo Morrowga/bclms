9528096a4329264d6bd8a3f31c66429c3b885591
thihaeung <154412231+jonathanag2024@users.noreply.github.com>
2024-02-19 18:11:58 +0630

    conditon fixed

--- src/BlendedConcept/Student/Application/Repositories/Eloquent/StudentRepository.php
+++ src/BlendedConcept/Student/Application/Repositories/Eloquent/StudentRepository.php
@@ -216,7 +216,7 @@ class StudentRepository implements StudentRepositoryInterface
             // } else
             $parent_id = null;
             $teacher_id = null;
-            
+
             if ($auth->name == 'BC Subscriber') {
                 if (auth()->user()->b2bUser == null) {
                     $subscription = auth()->user()->parents->subscription;
@@ -283,7 +283,7 @@ class StudentRepository implements StudentRepositoryInterface
             $createStudentEloquent->num_silver_coins = 0;
             $createStudentEloquent->save();
 
-            if ($auth->name != 'B2C Parent' && $auth->name != 'Both Parent') {
+            if($parent_id == null){
                 $createStudentEloquent->teachers()->sync([$teacher_id]);
             }
 
